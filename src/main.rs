aa6bfbb1611cd8963d1cde0f49d84be886e6154b
Matt Cook <mathew.cook@mastery.net>
2023-02-22 14:17:27 -0600

    control flow fundamentals

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,25 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let foo = add(2, 5);
-    println!("{:?}", foo);
+    let mut total = add(21, 5);
+    let mut free_shipping = false;
+
+    if total > 50 {
+        println!("You qualify for free shipping");
+        free_shipping = true;
+    }
+    else if total > 20 {
+        println!("If you add more items, you can qualify for free shipping!")
+    }
+    else {
+        println!("No free shipping")
+    }
+
+    total = match free_shipping  {
+        true => total + 0,
+        false => total + 5
+    };
+
+    println!("Your total was: {}", total);
 }
 
